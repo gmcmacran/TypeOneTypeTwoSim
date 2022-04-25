63964e895bc7ff2aeccf917738add4ec4c8fbeb6
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-04-25 16:53:29 -0500

    Adding exact tests

--- R Code/gaussian sim.R
+++ R Code/gaussian sim.R
@@ -1,6 +1,7 @@
 library(MLTesteR)
 library(tidyverse)
 library(stringr)
+library(EnvStats)
 
 ################
 # Type I
@@ -12,7 +13,6 @@ mus <- -3:3
 variances <- 1:5
 
 sim_results <- tibble()
-
 for (mu in mus) {
   for (variance in variances) {
     for (alt in c("two.sided", "less", "greater")) {
@@ -81,3 +81,77 @@ sim_results %>%
 # save
 sim_results %>%
   saveRDS("results/gaussian_type_one.rds")
+
+rm(sim_results, x, test, alt, mu, variance)
+
+sim_results_02 <- tibble()
+for (mu in mus) {
+  for (variance in variances) {
+    for (alt in c("two.sided", "less", "greater")) {
+      stats <- vector(mode = "numeric", length = B)
+      pvalues <- vector(mode = "numeric", length = B)
+      alts <- vector(mode = "character", length = B)
+      testName <- "t.test"
+      set.seed(1)
+      for (i in 1:B) {
+        x <- rnorm(n = N, mean = mu, sd = variance^.5)
+        test <- t.test(x = x, mu = mu, alternative = alt)
+        stats[i] <- test$statistic
+        pvalues[i] <- test$p.value
+        alts[i] <- test$alternative
+      }
+      temp <- tibble(test = testName, mu = mu, variance = variance, stat = stats, pvalue = pvalues, alt = alts)
+      sim_results_02 <- sim_results_02 %>% bind_rows(temp)
+      rm(stats, pvalues, alts, testName, temp, i)
+    }
+
+    for (alt in c("two.sided", "less", "greater")) {
+      stats <- vector(mode = "numeric", length = B)
+      pvalues <- vector(mode = "numeric", length = B)
+      alts <- vector(mode = "character", length = B)
+      testName <- "varTest"
+      set.seed(1)
+      for (i in 1:B) {
+        x <- rnorm(n = N, mean = mu, sd = variance^.5)
+        test <- varTest(x = x, sigma.squared = variance, alternative = alt)
+        stats[i] <- test$statistic
+        pvalues[i] <- test$p.value
+        alts[i] <- test$alternative
+      }
+      temp <- tibble(test = testName, mu = mu, variance = variance, stat = stats, pvalue = pvalues, alt = alts)
+      sim_results_02 <- sim_results_02 %>% bind_rows(temp)
+      rm(stats, pvalues, alts, testName, temp, i)
+    }
+  }
+}
+
+# Check structure
+sim_results_02 %>%
+  distinct(test) %>%
+  nrow() == 2
+
+sim_results_02 %>%
+  distinct(mu) %>%
+  nrow() == length(mus)
+
+sim_results_02 %>%
+  distinct(variance) %>%
+  nrow() == length(variances)
+
+sim_results_02 %>%
+  distinct(alt) %>%
+  nrow() == 3
+
+sim_results_02 %>%
+  pull(pvalue) %>%
+  min(na.rm = TRUE) >= 0
+
+sim_results_02 %>%
+  pull(pvalue) %>%
+  max(na.rm = TRUE) <= 1
+
+# save
+sim_results_02 %>%
+  saveRDS("results/gaussian_type_one_exact.rds")
+
+rm(list = ls())
