afbbd82abe57513dc8b76e7067f6d4d6a76bafba
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-04-25 09:27:24 -0500

    split sim across dist

---
+++ R Code/gaussian sim.R
@@ -0,0 +1,111 @@
+library(MLTesteR)
+library(tidyverse)
+library(stringr)
+
+################
+# Type I
+###############
+B <- 5000
+N <- 200
+
+mus <- -3:3
+variances <- 1:5
+
+sim_results <- tibble()
+
+for (mu in mus) {
+  for (variance in variances) {
+    for (alt in c("two.sided", "less", "greater")) {
+      stats <- vector(mode = "numeric", length = B)
+      pvalues <- vector(mode = "numeric", length = B)
+      alts <- vector(mode = "character", length = B)
+      testName <- "gaussian_mean_lr_test"
+      set.seed(1)
+      for (i in 1:B) {
+        x <- rnorm(n = N, mean = mu, sd = variance^.5)
+        test <- gaussian_mu_lr_test(x, mu, alt)
+        stats[i] <- test$statistic
+        pvalues[i] <- test$p.value
+        alts[i] <- test$alternative
+      }
+      temp <- tibble(test = testName, mu = mu, variance = variance, stat = stats, pvalue = pvalues, alt = alts)
+      sim_results <- sim_results %>% bind_rows(temp)
+      rm(stats, pvalues, alts, testName, temp, i)
+    }
+
+    for (alt in c("two.sided", "less", "greater")) {
+      stats <- vector(mode = "numeric", length = B)
+      pvalues <- vector(mode = "numeric", length = B)
+      alts <- vector(mode = "character", length = B)
+      testName <- "gaussian_variance_lr_test"
+      set.seed(1)
+      for (i in 1:B) {
+        x <- rnorm(n = N, mean = mu, sd = variance^.5)
+        test <- gaussian_variance_lr_test(x, variance, alt)
+        stats[i] <- test$statistic
+        pvalues[i] <- test$p.value
+        alts[i] <- test$alternative
+      }
+      temp <- tibble(test = testName, mu = mu, variance = variance, stat = stats, pvalue = pvalues, alt = alts)
+      sim_results <- sim_results %>% bind_rows(temp)
+      rm(stats, pvalues, alts, testName, temp, i)
+    }
+  }
+}
+
+# Check structure
+sim_results %>%
+  distinct(test) %>%
+  nrow() == 2
+
+sim_results %>%
+  distinct(mu) %>%
+  nrow() == length(mus)
+
+sim_results %>%
+  distinct(variance) %>%
+  nrow() == length(variances)
+
+sim_results %>%
+  distinct(alt) %>%
+  nrow() == 3
+
+sim_results %>%
+  pull(pvalue) %>%
+  min(na.rm = TRUE) >= 0
+
+sim_results %>%
+  pull(pvalue) %>%
+  max(na.rm = TRUE) <= 1
+
+sim_results %>%
+  saveRDS("results/gaussian_type_one.rds")
+
+temp <- sim_results %>%
+  group_by(test, alt, mu, variance) %>%
+  summarise(TypeI = mean(pvalue <= .05, na.rm = TRUE), meanStat = mean(stat, na.rm = TRUE), N = sum(!is.na(pvalue))) %>%
+  arrange(desc(TypeI))
+
+ggplot(temp, aes(x = factor(mu), y = TypeI)) +
+  geom_boxplot() +
+  geom_hline(yintercept = .05) +
+  scale_y_continuous(breaks = seq(0, 1, .05), limits = c(0, 1)) +
+  labs(x = "Mu", y = "Type I Error")
+
+ggplot(temp, aes(x = factor(variance), y = TypeI)) +
+  geom_boxplot() +
+  geom_hline(yintercept = .05) +
+  scale_y_continuous(breaks = seq(0, 1, .05), limits = c(0, 1)) +
+  labs(x = "Variance", y = "Type I Error")
+
+ggplot(temp, aes(x = factor(alt), y = TypeI)) +
+  geom_boxplot() +
+  geom_hline(yintercept = .05) +
+  scale_y_continuous(breaks = seq(0, 1, .05), limits = c(0, 1)) +
+  labs(x = "Alternative Hypothesis", y = "Type I Error")
+
+ggplot(temp, aes(x = factor(test), y = TypeI)) +
+  geom_boxplot() +
+  geom_hline(yintercept = .05) +
+  scale_y_continuous(breaks = seq(0, 1, .05), limits = c(0, 1)) +
+  labs(x = "Hypothesis Test", y = "Type I Error")
