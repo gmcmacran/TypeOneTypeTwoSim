851b7fb046c3607f1e1e76371d3094da0eb33e20
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-07-21 09:30:29 -0500

    split type I and type II

--- R Code/cauchy sim Type II.R
+++ R Code/cauchy sim Type II.R
@@ -9,93 +9,12 @@ B <- 5000
 N <- 200
 
 ################
-# Type I
-################
-locations <- seq(-4, 4, 2)
-scales <- c(1, 3, 5)
-
-location <- locations[1]
-scale <- scales[1]
-
-sim_results <- tibble()
-for (location in locations) {
-  for (scale in scales) {
-    for (alt in c("two.sided", "less", "greater")) {
-      stats <- vector(mode = "numeric", length = B)
-      pvalues <- vector(mode = "numeric", length = B)
-      alts <- vector(mode = "character", length = B)
-      testName <- "cauchy_location_lr_test"
-      set.seed(1)
-      for (i in 1:B) {
-        x <- rcauchy(N, location, scale)
-        test <- cauchy_location_lr_test(x, location, alt)
-        stats[i] <- test$statistic
-        pvalues[i] <- test$p.value
-        alts[i] <- test$alternative
-      }
-      temp <- tibble(test = testName, location = location, scale = scale, stat = stats, pvalue = pvalues, alt = alts)
-      sim_results <- sim_results %>% bind_rows(temp)
-      rm(stats, pvalues, alts, testName, temp, i)
-    }
-
-    for (alt in c("two.sided", "less", "greater")) {
-      stats <- vector(mode = "numeric", length = B)
-      pvalues <- vector(mode = "numeric", length = B)
-      alts <- vector(mode = "character", length = B)
-      testName <- "cauchy_scale_lr_test"
-      set.seed(1)
-      for (i in 1:B) {
-        x <- rcauchy(N, location, scale)
-        test <- cauchy_scale_lr_test(x, scale, alt)
-        stats[i] <- test$statistic
-        pvalues[i] <- test$p.value
-        alts[i] <- test$alternative
-      }
-      temp <- tibble(test = testName, location = location, scale = scale, stat = stats, pvalue = pvalues, alt = alts)
-      sim_results <- sim_results %>% bind_rows(temp)
-      rm(stats, pvalues, alts, testName, temp, i)
-    }
-  }
-}
-
-# Check structure
-sim_results %>%
-  distinct(test) %>%
-  nrow() == 2
-
-sim_results %>%
-  distinct(location) %>%
-  nrow() == length(locations)
-
-sim_results %>%
-  distinct(scale) %>%
-  nrow() == length(scales)
-
-sim_results %>%
-  distinct(alt) %>%
-  nrow() == 3
-
-sim_results %>%
-  pull(pvalue) %>%
-  min(na.rm = TRUE) >= 0
-
-sim_results %>%
-  pull(pvalue) %>%
-  max(na.rm = TRUE) <= 1
-
-# save
-sim_results %>%
-  saveRDS("results/cauchy_type_one.rds")
-
-rm(sim_results, x, test, alt, location, scale, locations, scales)
-
-################
 # Type II
 ################
 
 location0 <- 10
 scale0 <- 10
-locationEffectSizes <- seq(-3, 3, .5) %>%
+locationEffectSizes <- seq(-4, 4, 1) %>%
   round(2) %>%
   setdiff(0)
 
@@ -145,7 +64,7 @@ rm(alt, locationEffectSize, x)
 
 location0 <- 10
 scale0 <- 10
-scaleEffectSizes <- seq(-5, 5, 1) %>%
+scaleEffectSizes <- seq(-4, 4, 1) %>%
   setdiff(0)
 
 for (scaleEffectSize in scaleEffectSizes) {
