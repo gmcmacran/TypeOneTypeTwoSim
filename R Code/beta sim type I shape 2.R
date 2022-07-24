57a282358684edf9255b7f77ea30883524fc586d
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-07-24 14:20:00 -0500

    split beta type 1 shape 1 shape 2

--- R Code/beta sim type I shape 2.R
+++ R Code/beta sim type I shape 2.R
@@ -5,43 +5,15 @@ library(stringr)
 ################
 # Simulation settings
 ################
+compiler::enableJIT(3)
 B <- 5000
 N <- 200
 
 ################
 # Type I
 ################
-shape1s <- seq(1, 9, 2)
-shape2s <- seq(1, 9, 2)
-
-sim_results <- tibble()
-for (shape1 in shape1s) {
-  for (shape2 in shape2s) {
-    for (alt in c("two.sided", "less", "greater")) {
-      stats <- vector(mode = "numeric", length = B)
-      pvalues <- vector(mode = "numeric", length = B)
-      alts <- vector(mode = "character", length = B)
-      CI_LBs <- vector(mode = "numeric", length = B)
-      CI_UBs <- vector(mode = "numeric", length = B)
-      testName <- "beta_shape1_lr_test"
-      set.seed(1)
-      for (i in 1:B) {
-        x <- rbeta(N, shape1 = shape1, shape2 = shape2)
-        test <- beta_shape1_lr_test(x, shape1, alt)
-        stats[i] <- test$statistic
-        pvalues[i] <- test$p.value
-        alts[i] <- test$alternative
-        CI_LBs[i] <- test$conf.int[1]
-        CI_UBs[i] <- test$conf.int[2]
-      }
-      temp <- tibble(test = testName, shape1 = shape1, shape2 = shape2, stat = stats, pvalue = pvalues, alt = alts, CI_LB = CI_LBs, CI_UB = CI_UBs)
-      sim_results <- sim_results %>% bind_rows(temp)
-      rm(stats, pvalues, alts, testName, temp, i)
-    }
-  }
-}
-sim_results %>% saveRDS("results/beta_type_one_shape1.rds")
-rm(sim_results)
+shape1s <- seq(1, 9, 4)
+shape2s <- seq(1, 9, 4)
 
 sim_results <- tibble()
 for (shape1 in shape1s) {
@@ -70,12 +42,6 @@ for (shape1 in shape1s) {
   }
 }
 sim_results %>% saveRDS("results/beta_type_one_shape2.rds")
-rm(sim_results)
-
-sim_results <- bind_rows(
-  readRDS(file = "results/beta_type_one_shape1.rds"),
-  readRDS(file = "results/beta_type_one_shape2.rds")
-)
 
 # Check structure
 sim_results %>%
@@ -106,6 +72,6 @@ all(sim_results$CI_LB < sim_results$CI_UB)
 
 # save
 sim_results %>%
-  saveRDS("results/beta_type_one.rds")
+  saveRDS("results/beta_type_one_shape2.rds")
 
 rm(list = ls())
