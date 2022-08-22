89f6b9d876b26701ae01a41f952ddc3ecd2f2e20
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-08-22 18:04:11 -0500

    remove unneeded for loop over alts

--- R Code/two sample/beta two sample type I shape 1.R
+++ R Code/two sample/beta two sample type I shape 1.R
@@ -18,24 +18,22 @@ shape2s <- seq(1, 9, 4)
 sim_results <- tibble()
 for (shape1 in shape1s) {
   for (shape2 in shape2s) {
-    for (alt in c("two.sided", "less", "greater")) {
-      stats <- vector(mode = "numeric", length = B)
-      pvalues <- vector(mode = "numeric", length = B)
-      alts <- vector(mode = "character", length = B)
-      testName <- "beta_shape1_one_way"
-      set.seed(1)
-      for (i in 1:B) {
-        x <- rbeta(N, shape1 = shape1, shape2 = shape2)
-        fctr <- factor(c(rep("1", N / 2), rep("2", N / 2)), levels = c("1", "2"))
-        test <- beta_shape1_one_way(x, fctr)
-        stats[i] <- test$statistic
-        pvalues[i] <- test$p.value
-        alts[i] <- test$alternative
-      }
-      temp <- tibble(test = testName, shape1 = shape1, shape2 = shape2, stat = stats, pvalue = pvalues, alt = alts)
-      sim_results <- sim_results %>% bind_rows(temp)
-      rm(stats, pvalues, alts, testName, temp, i, fctr, x, test, alt)
+    stats <- vector(mode = "numeric", length = B)
+    pvalues <- vector(mode = "numeric", length = B)
+    alts <- vector(mode = "character", length = B)
+    testName <- "beta_shape1_one_way"
+    set.seed(1)
+    for (i in 1:B) {
+      x <- rbeta(N, shape1 = shape1, shape2 = shape2)
+      fctr <- factor(c(rep("1", N / 2), rep("2", N / 2)), levels = c("1", "2"))
+      test <- beta_shape1_one_way(x, fctr)
+      stats[i] <- test$statistic
+      pvalues[i] <- test$p.value
+      alts[i] <- test$alternative
     }
+    temp <- tibble(test = testName, shape1 = shape1, shape2 = shape2, stat = stats, pvalue = pvalues, alt = alts)
+    sim_results <- sim_results %>% bind_rows(temp)
+    rm(stats, pvalues, alts, testName, temp, i, fctr, x, test)
   }
 }
 rm(shape1, shape2)
