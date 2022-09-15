4629e56e3f6989550c1008f4f00b449e00274690
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-09-15 18:07:28 -0500

    better checks

--- R Code/sampling distribution/check type II.R
+++ R Code/sampling distribution/check type II.R
@@ -1,40 +1,32 @@
-# Simple script to explore type II results and run a few sanity checks.
-
 library(LRTesteR)
 library(tidyverse)
 
 ###############
-# Load data
+# one sample
 ###############
 load_df <- function(fn) {
+  if (str_detect(fn, "one") & !str_detect(fn, "_one_way") & !str_detect(fn, "exponential")) {
+    print(str_c("Possible Error. fn: ", fn))
+  }
   fn <- str_c("results/", fn, collapse = "")
   DF <- readRDS(fn)
   DF <- DF %>%
-    select(test, alt, stat)
+    select(test, alt, stat, pvalue, effectSize)
   return(DF)
 }
 
 fns <- c(
   "gaussian_type_two.rds",
-  "gaussian_type_two_one_way.rds",
   "gamma_type_two_rate.rds",
-  "gamma_type_two_rate_one_way.rds",
   "gamma_type_two_scale.rds",
-  "gamma_type_two_scale_one_way.rds",
   "gamma_type_two_shape.rds",
-  "gamma_type_two_shape_one_way.rds",
   "poisson_type_two.rds",
-  "poisson_type_two_one_way.rds",
   "beta_type_two.rds",
-  "beta_type_two_one_way.rds",
   "negative_binomial_type_two.rds",
-  "negative_binomial_type_two_one_way.rds",
   "exponential_type_two.rds",
-  "exponential_type_two_one_way.rds",
   "binomail_type_two.rds",
-  "binomail_type_two_one_way.rds",
   "cauchy_type_two.rds",
-  "cauchy_type_two_one_way.rds"
+  "inverse_gaussian_type_two.rds"
 )
 
 typeII <- map_dfr(fns, load_df)
@@ -46,7 +38,7 @@ typeII %>%
 
 typeII %>%
   distinct(test) %>%
-  nrow() == 26
+  nrow() == 16
 
 typeII %>%
   distinct(alt) %>%
@@ -57,7 +49,74 @@ typeII %>%
   summarise(minStat = min(stat), maxStat = max(stat))
 
 typeII %>%
-  filter(alt == "two.sided", stat < 0) %>%
+  filter(alt != "two.sided") %>%
+  summarise(minStat = min(stat), maxStat = max(stat))
+
+typeII %>%
+  filter(alt != "two.sided") %>%
+  group_by(test) %>%
+  summarise(minStat = min(stat), maxStat = max(stat)) %>%
+  arrange(test) %>%
+  print(n = Inf)
+
+typeII %>%
+  summarise(
+    P_LB = all(pvalue >= 0),
+    P_UB = all(pvalue <= 1)
+  )
+
+rm(list = ls())
+
+###############
+# two sample
+###############
+load_df <- function(fn) {
+  if (str_detect(fn, "one") & !str_detect(fn, "_one_way") & !str_detect(fn, "exponential")) {
+    print(str_c("Possible Error. fn: ", fn))
+  }
+  fn <- str_c("results/", fn, collapse = "")
+  DF <- readRDS(fn)
+  DF <- DF %>%
+    select(test, alt, stat, pvalue, effectSize)
+  return(DF)
+}
+
+fns <- c(
+  "gaussian_type_two_one_way.rds",
+  "gamma_type_two_rate_one_way.rds",
+  "gamma_type_two_scale_one_way.rds",
+  "gamma_type_two_shape_one_way.rds",
+  "poisson_type_two_one_way.rds",
+  "beta_type_two_one_way.rds",
+  "negative_binomial_type_two_one_way.rds",
+  "exponential_type_two_one_way.rds",
+  "binomail_type_two_one_way.rds",
+  "cauchy_type_two_one_way.rds",
+  "inverse_gaussian_type_two_one_way.rds"
+)
+
+typeII <- map_dfr(fns, load_df)
+
+typeII %>%
+  drop_na() %>%
+  nrow() == typeII %>%
   nrow()
+
+typeII %>%
+  distinct(test) %>%
+  nrow() == 16
+
+typeII %>%
+  distinct(alt) %>%
+  nrow() == 1
+
+typeII %>%
+  summarise(minStat = min(stat), maxStat = max(stat))
+
+typeII %>%
+  summarise(
+    P_LB = all(pvalue >= 0),
+    P_UB = all(pvalue <= 1)
+  )
 
 rm(list = ls())
