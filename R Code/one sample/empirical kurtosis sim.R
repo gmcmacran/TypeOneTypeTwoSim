0bb3cd38080686489bc418d02cb60569c4ebc3ca
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2026-07-16 20:08:19 -0500

    kurtosis distribution update

--- R Code/one sample/empirical kurtosis sim.R
+++ R Code/one sample/empirical kurtosis sim.R
@@ -6,21 +6,49 @@ library(furrr)
 ################
 # Simulation settings
 ################
-plan(multisession, workers = 8)
-compiler::enableJIT(3)
+plan(multisession, workers = 15) # Only 15 values in kurtosis_es
 B <- 5000
 N <- 500
 
-calc_df <- function(kurtosis) {
-  v <- 4 + (6 / kurtosis)
-  return(v)
+# Two-component normal scale mixture. With probability p, sd is s1.
+# Otherwise sd is s2. Solves for s1 given a target excess kurtosis.
+# Reachable ceiling is 3/p - 3. (57 for p = .05.)
+calc_s1 <- function(exkurt, p = .05, s2 = 1) {
+  q <- 1 - p
+  k3 <- exkurt + 3
+  a <- 3 * p - k3 * p^2
+  b <- -2 * k3 * p * q * s2^2
+  cc <- 3 * q * s2^4 - k3 * q^2 * s2^4
+  disc <- pmax(b^2 - 4 * a * cc, 0) # exactly zero at exkurt = 0; guard rounding
+  s1squared <- (-b + sqrt(disc)) / (2 * a)
+  return(sqrt(s1squared))
+}
+
+
+# Symmetric beta. Excess kurtosis is -6 / (2a + 3). Covers (-2, 0).
+calc_a <- function(exkurt) {
+  a <- -3 / exkurt - 3 / 2
+  return(a)
+}
+
+# Negative targets come from a symmetric beta. Positive (and zero)
+# targets come from the normal scale mixture. Both are symmetric and
+# have all moments finite, so any grid value is valid for the test.
+rkurt <- function(n, exkurt, p = .05) {
+  if (exkurt < 0) {
+    x <- rbeta(n, calc_a(exkurt), calc_a(exkurt))
+  } else {
+    s1 <- calc_s1(exkurt, p)
+    x <- rnorm(n, 0, ifelse(runif(n) < p, s1, 1))
+  }
+  return(x)
 }
 
 ################
 # Type I
 ################
-kurtosis_es <- seq(.1, 1.4, .1)
-all(calc_df(kurtosis_es) > 8) # Need all degrees of freedom above 8 so the 8th moment is finite.
+kurtosis_es <- c(seq(-1.2, -.2, .2), seq(0, 1.2, .2))
+stopifnot(all(kurtosis_es > -2), all(kurtosis_es < 3 / .05 - 3))
 
 run_sim <- function(kurtosis_es) {
   sim_results <- tibble()
@@ -34,7 +62,7 @@ run_sim <- function(kurtosis_es) {
       testName <- "empirical_kurtosis_one_sample"
       for (i in 1:B) {
         set.seed(i)
-        x <- rt(n = N, df = calc_df(kurtosis))
+        x <- rkurt(n = N, exkurt = kurtosis)
         test <- empirical_kurtosis_one_sample(x, kurtosis, alt)
         stats[i] <- test$statistic
         pvalues[i] <- test$p.value
@@ -100,7 +128,7 @@ run_sim <- function(kurtosisEffectSizes) {
         testName <- "empirical_kurtosis_one_sample"
         for (i in 1:B) {
           set.seed(i)
-          x <- rt(n = N, df = calc_df(1 + kurtosisEffectSize))
+          x <- rkurt(n = N, exkurt = 1 + kurtosisEffectSize)
           test <- empirical_kurtosis_one_sample(x, 1, alt)
           stats[i] <- test$statistic
           pvalues[i] <- test$p.value
@@ -118,7 +146,7 @@ run_sim <- function(kurtosisEffectSizes) {
         testName <- "empirical_kurtosis_one_sample"
         for (i in 1:B) {
           set.seed(i)
-          x <- rt(n = N, df = calc_df(1 + kurtosisEffectSize))
+          x <- rkurt(n = N, exkurt = 1 + kurtosisEffectSize)
           test <- empirical_kurtosis_one_sample(x, 1, alt)
           stats[i] <- test$statistic
           pvalues[i] <- test$p.value
@@ -130,7 +158,6 @@ run_sim <- function(kurtosisEffectSizes) {
       }
     }
   }
-  rm(alt, kurtosisEffectSize, x, test)
   return(sim_results)
 }
 
--- R Code/two sample/empirical kurtosis two sample.R
+++ R Code/two sample/empirical kurtosis two sample.R
@@ -1,43 +1,76 @@
 library(LRTesteR)
 library(tidyverse)
+library(furrr)
 
 ################
 # Simulation settings
 ################
-compiler::enableJIT(3)
+plan(multisession, workers = 13) # Only 13 values of kurtosis are tested
 B <- 5000
 N <- 500
 
-calc_df <- function(kurtosis) {
-  v <- 4 + (6 / kurtosis)
-  return(v)
+# Two-component normal scale mixture. With probability p, sd is s1.
+# Otherwise sd is s2. Solves for s1 given a target excess kurtosis.
+# Reachable ceiling is 3/p - 3. (57 for p = .05.)
+calc_s1 <- function(exkurt, p = .05, s2 = 1) {
+  q <- 1 - p
+  k3 <- exkurt + 3
+  a <- 3 * p - k3 * p^2
+  b <- -2 * k3 * p * q * s2^2
+  cc <- 3 * q * s2^4 - k3 * q^2 * s2^4
+  disc <- pmax(b^2 - 4 * a * cc, 0) # exactly zero at exkurt = 0; guard rounding
+  s1squared <- (-b + sqrt(disc)) / (2 * a)
+  return(sqrt(s1squared))
+}
+
+# Symmetric beta. Excess kurtosis is -6 / (2a + 3). Covers (-2, 0).
+calc_a <- function(exkurt) {
+  a <- -3 / exkurt - 3 / 2
+  return(a)
+}
+
+# Negative targets come from a symmetric beta. Positive (and zero)
+# targets come from the normal scale mixture. Both are symmetric and
+# have all moments finite, so any grid value is valid for the test.
+rkurt <- function(n, exkurt, p = .05) {
+  if (exkurt < 0) {
+    x <- rbeta(n, calc_a(exkurt), calc_a(exkurt))
+  } else {
+    s1 <- calc_s1(exkurt, p)
+    x <- rnorm(n, 0, ifelse(runif(n) < p, s1, 1))
+  }
+  return(x)
 }
 
 ################
 # Type I
 ################
-kurtosis_es <- seq(.1, 1.4, .1)
-all(calc_df(kurtosis_es) > 8) # Need all degrees of freedom above 8 so the 8th moment is finite.
+kurtosis_es <- c(seq(-1.2, -.2, .2), seq(0, 1.2, .2))
+stopifnot(all(kurtosis_es > -2), all(kurtosis_es < 3 / .05 - 3))
 
-sim_results <- tibble()
-for (kurtosis in kurtosis_es) {
-  stats <- vector(mode = "numeric", length = B)
-  pvalues <- vector(mode = "numeric", length = B)
-  alts <- vector(mode = "character", length = B)
-  testName <- "empirical_kurtosis_one_way"
-  for (i in 1:B) {
-    set.seed(i)
-    x <- rt(n = N, df = calc_df(kurtosis))
-    fctr <- factor(c(rep("1", N / 2), rep("2", N / 2)), levels = c("1", "2"))
-    test <- empirical_kurtosis_one_way(x, fctr)
-    stats[i] <- test$statistic
-    pvalues[i] <- test$p.value
-    alts[i] <- test$alternative
+run_sim <- function(kurtosis_es) {
+  sim_results <- tibble()
+  for (kurtosis in kurtosis_es) {
+    stats <- vector(mode = "numeric", length = B)
+    pvalues <- vector(mode = "numeric", length = B)
+    alts <- vector(mode = "character", length = B)
+    testName <- "empirical_kurtosis_one_way"
+    for (i in 1:B) {
+      set.seed(i)
+      x <- rkurt(n = N, exkurt = kurtosis)
+      fctr <- factor(c(rep("1", N / 2), rep("2", N / 2)), levels = c("1", "2"))
+      test <- empirical_kurtosis_one_way(x, fctr)
+      stats[i] <- test$statistic
+      pvalues[i] <- test$p.value
+      alts[i] <- test$alternative
+    }
+    temp <- tibble(test = testName, kurtosis = kurtosis, stat = stats, pvalue = pvalues, alt = alts)
+    sim_results <- sim_results %>% bind_rows(temp)
+    rm(stats, pvalues, alts, testName, temp, i, fctr, x, test)
   }
-  temp <- tibble(test = testName, kurtosis = kurtosis, stat = stats, pvalue = pvalues, alt = alts)
-  sim_results <- sim_results %>% bind_rows(temp)
-  rm(stats, pvalues, alts, testName, temp, i, fctr, x, test)
+  return(sim_results)
 }
+sim_results <- future_map_dfr(kurtosis_es, run_sim, .options = furrr_options(seed = TRUE))
 
 # Check structure
 sim_results %>%
@@ -64,37 +97,42 @@ sim_results %>%
 sim_results %>%
   saveRDS("results/empirical_kurtosis,_type_one_one_way.rds")
 
-rm(sim_results, kurtosis, kurtosis_es)
+rm(sim_results, run_sim, kurtosis_es)
 
 ################
 # Type II
 ################
-kurtosis <- .1
-kurtosisEffectSizes <- seq(.1, 1.3, .2) %>%
-  round(2)
+kurtosisEffectSizes <- round(seq(-.30, .30, .10), 2)
+kurtosisEffectSizes <- kurtosisEffectSizes[kurtosisEffectSizes != 0]
 
-all(calc_df(kurtosis) > 8)
-all(calc_df(kurtosis + kurtosisEffectSizes) > 8)
-
-sim_results <- tibble()
-for (kurtosisEffectSize in kurtosisEffectSizes) {
-  stats <- vector(mode = "numeric", length = B)
-  pvalues <- vector(mode = "numeric", length = B)
-  alts <- vector(mode = "character", length = B)
-  testName <- "empirical_kurtosis_one_way"
-  for (i in 1:B) {
-    set.seed(i)
-    x <- c(rt(n = N / 2, df = calc_df(kurtosis)), rt(n = N / 2, df = calc_df(kurtosis + kurtosisEffectSizes)))
-    fctr <- factor(c(rep("1", N / 2), rep("2", N / 2)), levels = c("1", "2"))
-    test <- empirical_kurtosis_one_way(x, fctr)
-    stats[i] <- test$statistic
-    pvalues[i] <- test$p.value
-    alts[i] <- test$alternative
+run_sim <- function(kurtosisEffectSizes) {
+  sim_results <- tibble()
+  for (kurtosisEffectSize in kurtosisEffectSizes) {
+    stats <- vector(mode = "numeric", length = B)
+    pvalues <- vector(mode = "numeric", length = B)
+    alts <- vector(mode = "character", length = B)
+    testName <- "empirical_kurtosis_one_way"
+    for (i in 1:B) {
+      set.seed(i)
+      if (kurtosisEffectSize >= 0) {
+        kurtosis <- 0
+      } else {
+        kurtosis <- -.5
+      }
+      x <- c(rkurt(n = N / 2, exkurt = kurtosis), rkurt(n = N / 2, exkurt = kurtosis + kurtosisEffectSize))
+      fctr <- factor(c(rep("1", N / 2), rep("2", N / 2)), levels = c("1", "2"))
+      test <- empirical_kurtosis_one_way(x, fctr)
+      stats[i] <- test$statistic
+      pvalues[i] <- test$p.value
+      alts[i] <- test$alternative
+    }
+    temp <- tibble(test = testName, effectSize = kurtosisEffectSize, stat = stats, pvalue = pvalues, alt = alts)
+    sim_results <- sim_results %>% bind_rows(temp)
+    rm(stats, pvalues, alts, testName, temp, i, fctr, x, kurtosisEffectSize, test)
   }
-  temp <- tibble(test = testName, effectSize = kurtosisEffectSize, stat = stats, pvalue = pvalues, alt = alts)
-  sim_results <- sim_results %>% bind_rows(temp)
-  rm(stats, pvalues, alts, testName, temp, i, fctr, x, kurtosisEffectSize, test)
+  return(sim_results)
 }
+sim_results <- future_map_dfr(kurtosisEffectSizes, run_sim, .options = furrr_options(seed = TRUE))
 
 # Check structure
 sim_results %>%
